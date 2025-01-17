50d1c1da794be2fdc1d322edfec72f2e9f97608d
narumij <narumij@gmail.com>
2025-01-18 01:43:51 +0900

    package platform

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 
 let package = Package(
   name: "swift-ac-memoize",
-  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
