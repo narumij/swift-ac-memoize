9f5fda066a8c34b611c09612ef63b0d405594af4
narumij <narumij@gmail.com>
2025-08-31 18:46:22 +0900

    dependency

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
       exact: "601.0.1"),
     .package(
       url: "https://github.com/narumij/swift-ac-collections",
-      exact: "0.1.42"),
+      from: "0.1.42"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
