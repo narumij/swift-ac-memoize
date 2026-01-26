7ddd76149e54c1054cef8ecae34930c7c4e82e2e
narumij <narumij@gmail.com>
2026-01-26 13:24:51 +0900

    change dependency

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
       exact: "602.0.0"),
     .package(
       url: "https://github.com/narumij/swift-ac-collections",
-      from: "0.2.0"),
+      branch: "compatible/AtCoder/2025"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
